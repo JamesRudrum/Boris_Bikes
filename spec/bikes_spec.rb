766605e7bac5e89d56057b0d0ba0d957850895f2
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-08 16:25:41 +0000

    test refactoing

--- spec/bikes_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'bikes'
-
-describe 'bikes' do
-  it 'describes a DockingStation' do
-    expect(describe_station()).to eq []
-  end
-end
---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,7 @@
+require 'docking_station'
+
+describe DockingStation do
+  # it 'describes a DockingStation' do
+  #   expect(describe_station()).to eq []
+  # end
+end
